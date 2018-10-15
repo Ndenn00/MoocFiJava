ea163f0fb5270cf45b29ecabd2ee20eec3a05377
Nial Dennison <ndennison01@qub.ac.uk>
2018-10-15 09:01:12 +0100

    finished no. 48

---
+++ mooc-2013-OOProgrammingWithJava-PART1/week3-045.Greatest/src/Greatest.java
@@ -0,0 +1,17 @@
+
+import java.util.Arrays;
+
+
+public class Greatest {
+
+    public static int greatest(int number1, int number2, int number3) {
+        int[] numbers = {number1, number2, number3}; 
+        Arrays.sort(numbers);
+        return numbers[2];
+    }
+
+    public static void main(String[] args) {
+        int result = greatest(2, 7, 3);
+        System.out.println("Greatest: " + result);
+    }
+}
